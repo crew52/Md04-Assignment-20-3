747cc43d41512d1d45fd530ca477b24c0a4300d0
crew52 <dungnc150602@gmail.com>
2025-03-19 22:42:40 +0700

    update all

---
+++ Assignment-Spring/src/test/java/codegym/c10/assignmentspring/AssignmentSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package codegym.c10.assignmentspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
